0255fa716e6c1499ae69617b429897209d19b791
LouisWongHK <LouisWongHK@protonmail.com>
2025-05-17 23:41:32 +0000

    npm run start

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Louis 👋</h1>
+      <p>Welcome to your portfolio!</p>
+    </div>
+  );
+}
+
+export default App;
